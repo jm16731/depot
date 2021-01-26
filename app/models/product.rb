5bbfcf20a6440bea33828160b42ac7c292c2d8b7
Greg Murrell <jmurrel1@my.westga.edu>
2021-01-26 16:14:08 -0500

    validates user submitted data

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    validates :title, :description, :image_url, presence: true
+    validates :title, uniqueness: true
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+    validates :image_url, allow_blank: true, format: {
+      with: %r{\.(gif|jpg|jpeg|png)\z}i,
+      message: 'must be a URL for a GIF, JPG, JPEG, or PNG image.'
+    }
 end
