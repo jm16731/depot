3a0fcfa114a5fc8e4e19e882cf252d6c690981f1
jmurrel1 <jmurrel1@cs6242-vm02.uwg.westga.edu>
2021-02-02 13:24:16 -0500

    Cart scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_190949) do
+ActiveRecord::Schema.define(version: 2021_02_02_182349) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
