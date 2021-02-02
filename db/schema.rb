4e8f4c973ab0b9c399cff740fbd76abd3d9791b2
jmurrel1 <jmurrel1@cs6242-vm02.uwg.westga.edu>
2021-02-02 16:15:53 -0500

    combine items in cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_210441) do
+ActiveRecord::Schema.define(version: 2021_02_02_211023) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
