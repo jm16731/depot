79638b9d6aa93e1067aa9006820620de6a92fc39
jmurrel1 <jmurrel1@cs6242-vm02.uwg.westga.edu>
2021-02-02 15:08:34 -0500

    task d, chapter 9 complete

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_items
+
     validates :title, :description, :image_url, presence: true
     validates :title, uniqueness: true
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -8,4 +12,13 @@ class Product < ApplicationRecord
     }
 
     validates :title, length: {minimum: 10}
+
+    private
+
+    def ensure_not_referenced_by_any_line_items
+      unless line_items.empty?
+        errors.add(:base, 'Line items present')
+        throw :abort
+      end
+    end
 end
