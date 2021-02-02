0e1227febe79bb2446bfc08fdbe9d57823294765
jmurrel1 <jmurrel1@cs6242-vm02.uwg.westga.edu>
2021-02-02 14:23:30 -0500

    added a test for task c views

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,6 +9,9 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'main table tbody tr', 3
+    assert_select '.actions ul li', 9
   end
 
   test "should get new" do
