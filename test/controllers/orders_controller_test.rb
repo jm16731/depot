2b93e926b19158fa83cdb91d579f77c7ca5c0439
Greg Murrell <j_greg_m@comcast.net>
2021-02-09 15:56:55 -0500

    task g1

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -11,6 +11,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+	post line_items_url, params: {product_id: products(:ruby).id}
     get new_order_url
     assert_response :success
   end
@@ -18,6 +19,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
   test "should create order" do
     assert_difference('Order.count') do
       post orders_url, params: { order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type } }
+	  assert_redirected_to store_index_url
     end
 
     assert_redirected_to order_url(Order.last)
@@ -44,5 +46,11 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to orders_url
+  end
+  
+  test "requires item in cart" do
+	get new_order_url
+	assert_redirected_to store_index_path
+	assert_equal flash[:notice], 'Your cart is empty'
   end
 end
