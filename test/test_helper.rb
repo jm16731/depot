1c755d666adc6f15c6e75f4c2c202d8107db9117
Greg Murrell <j_greg_m@comcast.net>
2021-02-14 18:07:34 -0500

    iteration j3 done

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,28 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+module AuthenticationHelpers
+	def login_as(user)
+		if respond_to? :visit
+			visit login_url
+			fill_in :name, with: user.name
+			fill_in :password, with: 'secret'
+			click_on 'Login'
+		else
+			post login_url, params: { name: user.name, password: 'secret' }
+		end
+	end
+	
+	def logout
+		delete logout_url
+	end
+	
+	def setup
+		login_as users(:one)
+	end
+end
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+class ActionDispatch::IntegrationTest
+	include AuthenticationHelpers
+end
 
-  # Add more helper methods to be used by all tests here...
+class ActionDispatch::SystemTestCase
+	include AuthenticationHelpers
 end
