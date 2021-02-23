6d6b1c7f4249c839016948d844101a75a8d7d340
Greg Murrell <j_greg_m@comcast.net>
2021-02-23 14:24:54 -0500

    renamed products channel js

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,6 +41,7 @@ class ProductsController < ApplicationController
       if @product.update(product_params)
         format.html { redirect_to @product, notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
+		
 		@products = Product.all.order(:title)
 		ActionCable.server.broadcast 'products', 
 			html: render_to_string('store/index', layout: false)
